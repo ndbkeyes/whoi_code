afac29094ed22d268f7153950f2f934f51d13127
Nash Keyes <ndbkeyes@gmail.com>
2021-06-14 11:40:06 -0400

    Isobath masking
    added third input to find_bottom - isobath depth to mask above. i.e. every profile whose depth is less than the isobath depth gets masked to NaN in the output bath_depth

--- bathymetry.py
+++ bathymetry.py
@@ -9,23 +9,30 @@ import numpy as np
 import xarray as xr
 
 
-def find_bottom(data,depth):
+# find depth of bottom based on NaNs in vertical profile
+def find_bottom(data,depth,isobath):
     
-    bath_mask = ~np.isnan(data)                 # find non-NaN observations
-    bath_count = np.count_nonzero(bath_mask)   # count non-NaN observations
+    # find & count non-NaN observations
+    bath_mask = ~np.isnan(data)               
+    bath_count = np.count_nonzero(bath_mask)   
     
+    # if location is on land, depth is NaN
     if bath_count == 0:
         bath_depth = np.nan
+        
     else:
-        bath_depth = depth[bath_count-1]            # find depth of bottom by index
+        
+        # get bottom depth by index
+        bath_depth = depth[bath_count-1]
+        
+        # if above 200m isobath, set to NaN
+        if bath_depth < isobath:
+            bath_depth = np.nan
     
     return bath_depth
     
 
-        
-        
-
-# filename for T (only using T bc NaN distribution is same in S)
+# load file for T (using T only, since NaN distribution is same in S)
 file = 'C:/Users/ndbke/Dropbox/_NDBK/Research/WHOI/WOA_data/woa18_A5B7_t00_04.nc'
 dat = xr.open_dataset(file,decode_times=False)
 
@@ -33,14 +40,7 @@ dat = xr.open_dataset(file,decode_times=False)
 dat = dat.isel(time=0)
 dat = dat.transpose("lat","lon","depth","nbounds")
 
-### testing on one (lat,lon) profile
-# test_profile = dat.t_an.sel(lat=0.125,lon=-60.125)
-# test_depth = test_profile.depth
-# find_bottom(test_profile.values,test_depth.values)
-# bd = xr.apply_ufunc(find_bottom,test_profile,test_depth,input_core_dims=[["depth"],["depth"]])
-# print(bd)
-
 # running over full dataset (via vectorization)
-full_bath = xr.apply_ufunc(find_bottom, dat.t_an, dat.depth, input_core_dims=[["depth"],["depth"]], vectorize=True)
+full_bath = xr.apply_ufunc(find_bottom, dat.t_an, dat.depth, 200, input_core_dims=[["depth"],["depth"],[]], vectorize=True)
 print(full_bath)
 full_bath.plot()
